3e855e863781e14c66fc88d7e1a47ffa35b4313d
sebaheg <sebastian@rebase.energy>
2024-10-06 10:51:50 +0200

    make benchmark plot

--- treen/utils.py
+++ treen/utils.py
@@ -6,11 +6,13 @@ def load_example(dataset_name):
     module_path = os.path.dirname(__file__)
 
     if dataset_name == "gefcom2014-wind":
-        data_path = os.path.join(module_path, 'data', 'gefcom2014', 'gefcom2014-wind.csv')
-        df = pd.read_csv(data_path, index_col=[0, 1], parse_dates=True, header=[0, 1])
+        df = pd.read_csv(os.path.join(module_path, 'data', 'gefcom2014', 'gefcom2014-wind.csv'),
+                        index_col=[0, 1], parse_dates=True, header=[0, 1])
+        df_scores = pd.read_csv(os.path.join(module_path, 'data', 'gefcom2014', 'gefcom2014-wind-scores.csv'),
+                                index_col=0)
     elif dataset_name == "solar":
         data_path = os.path.join(module_path, 'data', 'solar.csv')
     else:
         raise ValueError(f"Dataset '{dataset_name}' is not available.")
 
-    return df
+    return df, df_scores
