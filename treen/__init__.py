fc6a2c032edfb02a8f39ef5945413fbe6117eb77
sebaheg <sebastian@rebase.energy>
2024-11-06 14:34:12 +0000

    change name to treewe

--- treen/__init__.py
+++
@@ -1,6 +0,0 @@
-from .utils import load_example
-
-
-__version__ = '0.0.1'
-
-__all__ = ['load_example']
--- treen/examples/gefcom2014.py
+++
@@ -1,5 +0,0 @@
-from treen import load_example
-
-df = load_example("gefcom2014-wind")
-
-print(df.head())
